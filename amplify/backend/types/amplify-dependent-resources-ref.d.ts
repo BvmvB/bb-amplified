e6388c98ef31306aa8a2a7f98dcdb7310c01efec
Bogdan Balan <bogdan.balan@thinslices.com>
2024-01-08 14:34:32 +0200

    Add new files and configurations

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "bbamplified": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
